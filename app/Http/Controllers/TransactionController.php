584db09b85b1fbda8ed8b0653a03fe96e898758c
Yefta Steven Marcellius <yeftasm@gmail.com>
2023-10-23 19:24:22 +0700

    Create input data

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -20,7 +20,7 @@ class TransactionController extends Controller
      */
     public function create()
     {
-        //
+        return view('transactions.create');
     }
 
     /**
@@ -28,7 +28,21 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'amount' => 'required|numeric',
+            'type' => 'required|string',
+            'category' => 'required|string',
+            'notes' => 'required|string',
+        ]);
+
+        $transaction = Transaction::create([
+            'amount' => $validated['amount'],
+            'type' => $validated['type'],
+            'category' => $validated['category'],
+            'notes' => $validated['notes'],
+        ]);
+
+        return $transaction;
     }
 
     /**
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'amount',
+        'type',
+        'category',
+        'notes',
+    ];
 }
